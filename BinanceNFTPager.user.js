21d4bd8a9a4bbda8441e7f81f64e104dd8ec2e19
Tony Tkachenko <103267335+tonytkachenko@users.noreply.github.com>
2022-06-03 17:03:29 +0300

    Fixed updateUrl

--- BinanceNFTPager.user.js
+++ BinanceNFTPager.user.js
@@ -1,50 +1,51 @@
 // ==UserScript==
-// @name         		Binance NFT ID Pager
-// @namespace    		https://github.com/tonytkachenko/
-// @version      		1.0
-// @date        		2022-05-30
-// @description		Adds product id switcher on the pages of the marketplace and mystery boxes.
-// @description:ru	Добавляет переключение товаров на страницах маркетплейса и мистерибоксов.
-// @author       		Tony Tkachenko
-// @match        		https://www.binance.com/ru/nft/goods/blindBox/detail?*
-// @match        		https://www.binance.com/ru/nft/goods/detail?*
-// @icon         		https://www.google.com/s2/favicons?sz=64&domain=binance.com
-// @updateURL			https://raw.githubusercontent.com/tonytkachenko/userscripts/BinanceNFTPager.meta.js
-// @downloadURL		https://raw.githubusercontent.com/tonytkachenko/userscripts/BinanceNFTPager.user.js
-// @supportURL			https://github.com/tonytkachenko/userscripts/issues
-// @source				https://github.com/tonytkachenko/userscripts/BinanceNFTPager.user.js
-// @grant        		none
+// @name             Binance NFT ID Pager
+// @namespace        https://github.com/tonytkachenko/
+// @version          1.0
+// @date             2022-05-30
+// @description      Adds product id switcher on the pages of the marketplace and mystery boxes.
+// @description:ru   Добавляет переключение товаров на страницах маркетплейса и мистерибоксов.
+// @author           Tony Tkachenko
+// @match            https://www.binance.com/ru/nft/goods/blindBox/detail?*
+// @match            https://www.binance.com/ru/nft/goods/detail?*
+// @icon             https://www.google.com/s2/favicons?sz=64&domain=binance.com
+// @updateURL        https://raw.githubusercontent.com/tonytkachenko/userscripts/main/BinanceNFTPager.meta.js
+// @downloadURL      https://raw.githubusercontent.com/tonytkachenko/userscripts/main/BinanceNFTPager.user.js
+// @supportURL       https://github.com/tonytkachenko/userscripts/issues
+// @source           https://github.com/tonytkachenko/userscripts/BinanceNFTPager.user.js
+// @grant            none
 // ==/UserScript==
 
+
 (() => {
-   'use strict';
+  "use strict";
 
-   const buttons = [
-      { text: "+1 ID", value: 1 },
-      { text: "+10 ID", value: 10 },
-      { text: "+100 ID", value: 100 },
-      { text: "+500 ID", value: 500 },
-      { text: "+1000 ID", value: 1000 },
-   ];
+  const buttons = [
+    { text: "+1 ID", value: 1 },
+    { text: "+10 ID", value: 10 },
+    { text: "+100 ID", value: 100 },
+    { text: "+500 ID", value: 500 },
+    { text: "+1000 ID", value: 1000 },
+  ];
 
-   const buttonClick = (event) => {
-      event.stopPropagation();
+  const buttonClick = (event) => {
+    event.stopPropagation();
 
-      let url = new URL(window.location.href);
-      let params = url.searchParams;
+    let url = new URL(window.location.href);
+    let params = url.searchParams;
 
-      var productId = Number(params.get('productId'));
-      productId += Number(event.currentTarget.value);
-      params.set('productId', productId);
+    var productId = Number(params.get("productId"));
+    productId += Number(event.currentTarget.value);
+    params.set("productId", productId);
 
-      url.search = params.toString();
+    url.search = params.toString();
 
-      location.href = url.href;
-   };
+    location.href = url.href;
+  };
 
-   const initStyles = () => {
-      const styleElement = document.createElement('style');
-      styleElement.innerText = `#buttons-container {
+  const initStyles = () => {
+    const styleElement = document.createElement("style");
+    styleElement.innerText = `#buttons-container {
          position:fixed;
          left:2px;
          top:1px;
@@ -70,26 +71,26 @@
          cursor: pointer;
       }`;
 
-      document.getElementsByTagName("head")[0].appendChild(styleElement);
-   };
+    document.getElementsByTagName("head")[0].appendChild(styleElement);
+  };
 
-   const initButtons = () => {
-      const container = document.createElement('div');
-      container.id = "buttons-container";
+  const initButtons = () => {
+    const container = document.createElement("div");
+    container.id = "buttons-container";
 
-      buttons.forEach(element => {
-         let button = document.createElement('button');
-         button.innerText = element.text;
-         button.value = element.value;
-         button.addEventListener("click", buttonClick);
-         container.appendChild(button);
-      });
+    buttons.forEach((element) => {
+      let button = document.createElement("button");
+      button.innerText = element.text;
+      button.value = element.value;
+      button.addEventListener("click", buttonClick);
+      container.appendChild(button);
+    });
 
-      document.body.prepend(container);
-   };
+    document.body.prepend(container);
+  };
 
-   (() => {
-      initStyles();
-      initButtons();
-   })();
+  (() => {
+    initStyles();
+    initButtons();
+  })();
 })();
